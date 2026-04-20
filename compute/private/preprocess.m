9e10d1b9d0c1161f8b49daa6c4819223d4642b87
pietro <pietro.bozzo1998@gmail.com>
2026-04-20 17:39:44 +0200

    move to private to avoid name conflicts

--- compute/private/preprocess.m
+++ compute/private/preprocess.m
@@ -1,8 +1,7 @@
 function y = preprocess(x)
-    %remove outlier that appear only once
+    % remove outliers that appear only once
     uniqueVals = unique(x);
     [counts, ~] = histcounts(x, [uniqueVals; max(uniqueVals) + 1]);
     moreThanOnce = uniqueVals(counts > 1);
     y = x(ismember(x, moreThanOnce));
 end
-
