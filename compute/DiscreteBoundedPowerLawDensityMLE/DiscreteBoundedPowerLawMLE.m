842d7e7e4faf8ca43bd70d2d245c642f43e5be21
pietro <pietro.bozzo1998@gmail.com>
2026-04-22 18:14:21 +0200

    update

--- compute/DiscreteBoundedPowerLawDensityMLE/DiscreteBoundedPowerLawMLE.m
+++ compute/DiscreteBoundedPowerLawDensityMLE/DiscreteBoundedPowerLawMLE.m
@@ -1,32 +1,32 @@
-function alpha = DiscreteBoundedPowerLawMLE(x, xmin, xmax, step)
-    % perform MLE maximization to find the best power law parameter on raw data x, with the constrain xmin and xmax
-    X = x(x >= xmin & x <= xmax);
-    n = length(X);
-    l1 = sum(log(X));
-    t = xmin:xmax;
-    log_t = log(t);
+function alpha = DiscreteBoundedPowerLawMLE(x,xmin,xmax,step)
+% perform MLE maximization to find the best power law parameter on data 'x', with the constraint xmin and xmax
 
-    upper = 6;
-    lower = 1.1;
-    
-    % the dichotomy looks for the 0 of the log Likelihood derivative (see docs/powerLawFit.md)
-    for i = 1:step
-        mid = (upper + lower)/2;
-        D = derivative(n, l1, t, log_t, mid);
-        if D > 0
-            lower = mid;
-        else
-            upper = mid;
-        end
-    end
-    alpha = (upper + lower)/2;
+X = x(x >= xmin & x <= xmax);
+n = length(X);
+l1 = sum(log(X));
+
+t = (xmin : xmax).';
+log_t = log(t);
+
+lower = 1.1;
+upper = 6;
+
+% dichotomy to find 0 of the log-Likelihood derivative (see docs/powerLawFit.md)
+for i = 1 : step
+  mid = (upper + lower) / 2;
+
+  % derivative
+  log_w = -mid * log_t;
+  w = exp(log_w - max(log_w)); % avoid overflow in exp
+  l2 = sum(log_t .* w);
+  l3 = sum(w);
+  D = -l1 + n * l2 / l3;
+
+  if D > 0
+    lower = mid;
+  else
+    upper = mid;
+  end
 end
 
-% --- helper functions ---
-
-function D = derivative(n, l1, t, log_t, alpha)
-    t_alpha = t.^(-alpha);
-    l2 = sum(log_t.*t_alpha);
-    l3 = sum(t_alpha);
-    D = -l1 + n * l2 / l3;
-end
+alpha = (upper + lower) / 2;
